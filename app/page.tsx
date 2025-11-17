16d6dd9756e2fbffc769023ee3d08f952ff2f1c9
smit dhimar <smitdhimar61@gmail.com>
2025-11-16 22:13:23 -0500

    feat: successful socket client established

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+"use client";
+
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [num, setNum] = useState(undefined);
+
+  useEffect(() => {
+    const socket = io("http://localhost:4000");
+
+    socket.on("connect", () => {
+      console.log("CLIENT CONNECTED:", socket.id);
+      
+    });
+
+    socket.on("number", (value) => {
+      console.log("RECEIVED NUMBER:", value);
+      setNum(value);
+    });
+
+    socket.on("connect_error", (err) => {
+      console.log("CONNECT ERROR:", err.message);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <h1 className="bg-red-500">hello</h1>
+    <div>
+      {num !== undefined
+        ? `Server sent ${num}`
+        : `No number was sent yet`}
     </div>
   );
 }
