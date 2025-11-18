f03c8f411cf5de83346ff262ea2455eb963d02ff
smit dhimar <smitdhimar61@gmail.com>
2025-11-17 23:00:28 -0500

    refactor: change home page code to home page and create a separate socket client connection page

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,8 @@
-"use client";
-
-import io from "socket.io-client";
-import { useState, useEffect } from "react";
-
 export default function Home() {
-  const [num, setNum] = useState(undefined);
-
-  useEffect(() => {
-    const connectToSocket = async()=>{
-
-      try {
-        const response = await fetch('api/socket-url')
-        const result = await response.json();
-        console.log(result)
-        if(!result?.socketUrl) {
-          console.log("No socket url found");
-          throw new Error("No socket url found")
-        }
-        // debugger
-        const socket = io(result.socketUrl, {
-          reconnection: false
-        });
-    
-        socket.on("connect", () => {
-          console.log("CLIENT CONNECTED:", socket.id);
-    
-        });
-    
-        socket.on("number", (value) => {
-          console.log("RECEIVED NUMBER:", value);
-          setNum(value);
-        });
-    
-        socket.on("connect_error", (err) => {
-          console.log("CONNECT ERROR:", err.message);
-          setNum(undefined)
-        });
-        return () => {
-          socket.disconnect();
-        };
-      } catch (error: any) {
-        console.log("There is some error connecting to server: ", error?.message)
-      }
-    }
-    connectToSocket();
-  }, []);
 
   return (
     <div>
-      {num !== undefined
-        ? `Server sent ${num}`
-        : `No number was sent yet`}
+     Home page
     </div>
   );
 }
