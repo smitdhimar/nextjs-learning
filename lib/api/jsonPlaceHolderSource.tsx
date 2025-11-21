d844f0dd42c2306b067abeafc402cab2ce0fc1a0
smit dhimar <smitdhimar61@gmail.com>
2025-11-20 22:59:49 -0500

    style: change formatting of jsonplaceholder gateway's getPosts() funtion"

--- lib/api/jsonPlaceHolderSource.tsx
+++ lib/api/jsonPlaceHolderSource.tsx
@@ -1,6 +1,11 @@
 import { apiClient } from "./apiClient";
 
 const baseUrl = process.env.NEXT_PUBLIC_JSON_PLACEHOLDER_GATEWAY;
-export const getPosts = (postId?:Number) =>{
-   return apiClient(`/posts/${postId || ''}`, {baseUrl: baseUrl});
-}
+export const getPosts = (postId?: Number) => {
+  return apiClient(`/posts/${postId || ""}`, {
+    baseUrl: baseUrl,
+    next: {
+      revalidate: 30,
+    },
+  });
+};
